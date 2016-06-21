03c9327fac8974048d9eb04bed3e50f7baf93c59
周文 <wen3.zw3@alibaba-inc.com>
2016-06-21 13:39:13 +0800

    update:add loger

--- app/src/main/java/com/chowen/lightutils/MainActivity.java
+++ app/src/main/java/com/chowen/lightutils/MainActivity.java
@@ -1,7 +1,9 @@
 package com.chowen.lightutils;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.chowen.lightutils.log.Loger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Loger.d("chowen");
     }
 }
