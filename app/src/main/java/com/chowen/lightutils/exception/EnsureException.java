af476f807143fb32f3303b141cc949ee8b729527
周文 <wen3.zw3@alibaba-inc.com>
2016-06-28 14:42:11 +0800

    update:rename class

--- app/src/main/java/com/chowen/lightutils/exception/EnsureException.java
+++ app/src/main/java/com/chowen/lightutils/exception/EnsureException.java
@@ -8,15 +8,15 @@ import android.util.Log;
  * @version 0.1
  * @since 2016/4/8
  */
-public class CheckException {
+public class EnsureException extends BaseException{
 
-    private final static String TAG = CheckException.class.getSimpleName();
+    private final static String TAG = EnsureException.class.getSimpleName();
 
     /**
      * check NPE
      * @param params params
      */
-    public static void checkNPE(Object params) {
+    public static void ensureNPE(Object params) {
         if (params == null) {
             Log.e(TAG, "params can not null");
             throw new NullPointerException("params is not be null");
