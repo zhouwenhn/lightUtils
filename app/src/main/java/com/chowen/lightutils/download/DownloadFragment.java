6ab99c35baeb2b6ddbb13df08e559c179afa81f9
周文 <wen3.zw3@alibaba-inc.com>
2016-07-27 18:03:38 +0800

    add download

---
+++ app/src/main/java/com/chowen/lightutils/download/DownloadFragment.java
@@ -0,0 +1,23 @@
+package com.chowen.lightutils.download;
+
+import com.chowen.cn.library.ioc.annotations.field.InjectContentView;
+import com.chowen.lightutils.R;
+import com.chowen.lightutils.base.BaseFragment;
+
+/**
+ * Created by zhouwen on 16/7/23.
+ */
+
+@InjectContentView(value = R.layout.download_fragment)
+public class DownloadFragment extends BaseFragment {
+
+//    @InjectChildView(value = R.id.btn, listener = View.OnClickListener.class)
+//    private Button mBtn;
+
+
+//    @Override
+//    public void onClick(View v) {
+//        Toast.makeText(getActivity(), "return activity!", Toast.LENGTH_LONG).show();
+//        getActivity().onBackPressed();
+//    }
+}
