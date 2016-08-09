ed532c462506cd51a020c7f69459d226d5e1031e
周文 <wen3.zw3@alibaba-inc.com>
2016-08-09 09:41:45 +0800

    opt toast

--- library/src/main/java/com/chowen/cn/library/toast/ToastUtil.java
+++ library/src/main/java/com/chowen/cn/library/toast/ToastUtil.java
@@ -1,6 +1,8 @@
 package com.chowen.cn.library.toast;
 
+import android.content.Context;
 import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,54 +18,54 @@ import com.chowen.cn.library.LibApplication;
  */
 public class ToastUtil {
 
-    public static void showToast(String msg){
-        Toast.makeText(LibApplication.getInstance().getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    public static void showToast(@NonNull Context context, String msg){
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
     }
 
-    public static void showToast(@StringRes int msgId){
-        String msg = LibApplication.getInstance().getResources().getString(msgId);
-        showToast(msg);
+    public static void showToast(@NonNull Context context, @StringRes int msgId){
+        String msg = context.getResources().getString(msgId);
+        showToast(context, msg);
     }
 
-    public static void showToast(String msg, int duration){
-        Toast toast = new Toast(LibApplication.getInstance().getApplicationContext());
+    public static void showToast(@NonNull Context context, String msg, int duration){
+        Toast toast = new Toast(context);
         toast.setDuration(duration);
         toast.show();
     }
 
-    public static void showToast(@StringRes int msgId, int duration){
-        String msg = LibApplication.getInstance().getResources().getString(msgId);
-        showToast(msg, duration);
+    public static void showToast(@NonNull Context context, @StringRes int msgId, int duration){
+        String msg = context.getResources().getString(msgId);
+        showToast(context, msg, duration);
     }
 
-    public static void showToast(View view, String msg, int duration){
-        Toast toast = new Toast(LibApplication.getInstance().getApplicationContext());
+    public static void showToast(@NonNull Context context, View view, String msg, int duration){
+        Toast toast = new Toast(context);
         toast.setText(msg);
         toast.setDuration(duration);
         toast.setView(view);
         toast.show();
     }
 
-    public static void showToast(@LayoutRes int viewRes, String msg, int duration){
-        LayoutInflater layoutInflater = LayoutInflater.from(LibApplication.getInstance().getApplicationContext());
+    public static void showToast(@NonNull Context context, @LayoutRes int viewRes, String msg, int duration){
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
         View view = layoutInflater.inflate(viewRes, null);
-        showToast(view, msg, duration);
+        showToast(context, view, msg, duration);
     }
 
-    public static void showToast(@LayoutRes int viewRes, @StringRes int msgId, int duration){
-        LayoutInflater layoutInflater = LayoutInflater.from(LibApplication.getInstance().getApplicationContext());
+    public static void showToast(@NonNull Context context, @LayoutRes int viewRes, @StringRes int msgId, int duration){
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
         View view = layoutInflater.inflate(viewRes, null);
         String msg = LibApplication.getInstance().getResources().getString(msgId);
-        showToast(view, msg, duration);
+        showToast(context, view, msg, duration);
     }
 
-    public static void showToast(@StringRes int msgId, int gravity,int xOffset, int yOffset){
-        String msg = LibApplication.getInstance().getResources().getString(msgId);
-        showToast(msg, gravity, xOffset, yOffset);
+    public static void showToast(@NonNull Context context, @StringRes int msgId, int gravity,int xOffset, int yOffset){
+        String msg = context.getResources().getString(msgId);
+        showToast(context, msg, gravity, xOffset, yOffset);
     }
 
-    public static void showToast(String msg, int gravity, int xOffset, int yOffset){
-        Toast toast = new Toast(LibApplication.getInstance().getApplicationContext());
+    public static void showToast(@NonNull Context context, String msg, int gravity, int xOffset, int yOffset){
+        Toast toast = new Toast(context);
         toast.setGravity(gravity, xOffset, yOffset);
         toast.setText(msg);
         toast.show();
