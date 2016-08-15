a80db0041070738964d80e4bfb4cabdbe5be6fec
周文 <wen3.zw3@alibaba-inc.com>
2016-08-15 11:02:13 +0800

    add utils

---
+++ library/src/main/java/com/chowen/cn/library/utils/FileUtils.java
@@ -0,0 +1,13 @@
+package com.chowen.cn.library.utils;
+
+/**
+ * Created by zhouwen on 16/8/14.
+ */
+public class FileUtils {
+
+    private FileUtils() {
+        throw new UnsupportedOperationException("is static, un supported operation exception");
+    }
+
+
+}
